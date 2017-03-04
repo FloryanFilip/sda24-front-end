a7f37d7f1a50f1180c1da147f5c31422d0ade837
brogowski77 <brogowski77@gmail.com>
2017-03-04 12:04:04 +0100

    added test queries

--- public/app.js
+++ public/app.js
@@ -1,7 +1,12 @@
 angular.module("SdaApp", []).controller("PhrasesCtrl", function ($scope, $http) {
+    $http.get("https://jsonplaceholder.typicode.com/users").then(function (response) {
+        $scope.queries = response.data;
 
+    })
+    //$scope.queries= ["quwey1", "testr", "other"];
     $scope.search = function() {
         $http.post("https://sda24backend.herokuapp.com/", {query: $scope.query});
+        //$scope.message = $scope.query;
 
         alert($scope.query);
     };
