92fb8a382f1932b8fcde949501f3a6797a47f2cb
brogowski77 <brogowski77@gmail.com>
2017-03-04 12:52:12 +0100

    ui-router

--- public/app.js
+++ public/app.js
@@ -1,14 +1,27 @@
-angular.module("SdaApp", []).controller("PhrasesCtrl", function ($scope, $http) {
+angular.module("SdaApp", ['ui.router']).controller("PhrasesCtrl", function ($scope, $http) {
     $http.get("https://jsonplaceholder.typicode.com/users").then(function (response) {
         $scope.queries = response.data;
 
-    })
+    });
     //$scope.queries= ["quwey1", "testr", "other"];
-    $scope.search = function() {
+    $scope.search = function () {
         $http.post("https://sda24backend.herokuapp.com/", {query: $scope.query});
         //$scope.message = $scope.query;
 
         alert($scope.query);
     };
 
+}).config(function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state({
+            name: "query",
+            url: "/",
+            templateUrl: "templates/query.html"
+        }).state({
+            name: "details",
+            url: "/details",
+            templateUrl: "templates/details.html"
+        }
+    )
 });
